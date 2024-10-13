94c1ae291d1841eb4b6c2ef9262280942c9f0714
Aly-exe <alibnraslan@gmail.com>
2024-10-13 07:44:56 +0200

    finish Home Screen Page

--- lib/features/quiz/presentation/view/home_page_screen.dart
+++ lib/features/quiz/presentation/view/home_page_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:quizapp/features/quiz/presentation/view/widgets/lets_start_button_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +8,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home Page"),),
+      appBar: AppBar(
+          title: const Text(
+            'Quiz App',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent),
+      body: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Image.asset(
+              "assets/images/question_image.png",
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            LetsStart()
+          ]),
     );
   }
 }

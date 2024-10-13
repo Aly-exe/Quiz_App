c75ab73ccfd02b90fbeb99577656071c939db964
Aly-exe <alibnraslan@gmail.com>
2024-10-13 07:45:22 +0200

    add some questions

---
+++ lib/features/quiz/data/models/questions_list.dart
@@ -0,0 +1,158 @@
+import 'package:quizapp/features/quiz/data/models/question_model.dart';
+
+List<Question> questionsList = [
+  Question(
+    question: "من هو مؤسس شركة مايكروسوفت؟",
+    options: {
+      false: "مارك زوكربيرغ",
+      true: "بيل غيتس",
+      false: "ستيف جوبز",
+      false: "إيلون ماسك"
+    },
+  ),
+  Question(
+    question: "من هو أول رئيس للولايات المتحدة؟",
+    options: {
+      false: "توماس جيفرسون",
+      false: "جون آدامز",
+      true: "جورج واشنطن",
+      false: "أبراهام لينكولن"
+    },
+  ),
+  Question(
+    question: "ما هو العنصر الكيميائي الذي يرمز له بالرمز 'O'؟",
+    options: {
+      false: "الهيدروجين",
+      true: "الأكسجين",
+      false: "الكربون",
+      false: "النيتروجين"
+    },
+  ),
+  Question(
+    question: "ما هي عاصمة مصر؟",
+    options: {
+      true: "القاهرة",
+      false: "الإسكندرية",
+      false: "الأقصر",
+      false: "أسوان"
+    },
+  ),
+  Question(
+    question: "ما هي أطول سلسلة جبال في العالم؟",
+    options: {
+      false: "جبال الهيمالايا",
+      true: "جبال الأنديز",
+      false: "جبال الألب",
+      false: "جبال الروكي"
+    },
+  ),
+  Question(
+    question: "ما هو أكبر كوكب في المجموعة الشمسية؟",
+    options: {false: "زحل", false: "المريخ", false: "الأرض", true: "المشتري"},
+  ),
+  Question(
+    question: "أين يقع برج إيفل؟",
+    options: {true: "باريس", false: "لندن", false: "نيويورك", false: "برلين"},
+  ),
+  Question(
+    question: "من هو مكتشف قانون الجاذبية؟",
+    options: {
+      false: "غاليليو غاليلي",
+      true: "إسحاق نيوتن",
+      false: "ألبرت أينشتين",
+      false: "نيكولا تيسلا"
+    },
+  ),
+  Question(
+    question: "ما هو أسرع حيوان على وجه الأرض؟",
+    options: {false: "النمر", true: "الفهد", false: "الظبي", false: "الصقر"},
+  ),
+  Question(
+    question: "من كتب رواية 'البؤساء'؟",
+    options: {
+      false: "تشارلز ديكنز",
+      true: "فيكتور هوجو",
+      false: "ليو تولستوي",
+      false: "إرنست همنغواي"
+    },
+  ),
+  Question(
+    question: "من هو مخترع المصباح الكهربي؟",
+    options: {
+      false: "ألبرت أينشتين",
+      true: "توماس أديسون",
+      false: "أحمد زويل",
+      false: "فيثاغورث"
+    },
+  ),
+  Question(
+    question: "من هو قائد المسلمين في معركة بدر؟",
+    options: {
+      false: "عمر بن الخطاب",
+      false: "أبو بكر الصديق",
+      false: "علي بن أبي طالب",
+      true: "النبي محمد"
+    },
+  ),
+  Question(
+    question: "ما هو العنصر الكيميائي الذي يرمز له بالرمز 'H'؟",
+    options: {
+      false: "الأكسجين",
+      false: "النيتروجين",
+      true: "الهيدروجين",
+      false: "الكربون"
+    },
+  ),
+  Question(
+    question: "ما هي اللغة الرسمية في البرازيل؟",
+    options: {
+      false: "الفرنسية",
+      false: "الإنجليزية",
+      false: "الإسبانية",
+      true: "البرتغالية"
+    },
+  ),
+  Question(
+    question: "ما هي عملة اليابان؟",
+    options: {false: "اليورو", false: "الدولار", true: "الين", false: "الروبل"},
+  ),
+  Question(
+    question: "في أي عام وقعت الحرب العالمية الأولى؟",
+    options: {true: "1914", false: "1945", false: "1918", false: "1939"},
+  ),
+  Question(
+    question: "ما هي اللغة الرسمية في كندا؟",
+    options: {
+      false: "الفرنسية",
+      false: "الإسبانية",
+      true: "الإنجليزية",
+      false: "الألمانية"
+    },
+  ),
+  Question(
+    question: "ما هو الحيوان الذي يطلق عليه ملك الغابة؟",
+    options: {false: "النمر", true: "الأسد", false: "الذئب", false: "الدب"},
+  ),
+  Question(
+    question: "ما هو الغاز الذي يشكل النسبة الأكبر في الغلاف الجوي للأرض؟",
+    options: {
+      false: "الأكسجين",
+      false: "ثاني أكسيد الكربون",
+      false: "الأرجون",
+      true: "النيتروجين"
+    },
+  ),
+  Question(
+    question: "في أي قارة تقع دولة الأرجنتين؟",
+    options: {
+      false: "أوروبا",
+      false: "آسيا",
+      false: "أفريقيا",
+      true: "أمريكا الجنوبية"
+    },
+  ),
+  Question(
+    question: "ما هو عدد الكواكب في المجموعة الشمسية؟",
+    options: {true: "ثمانية", false: "سبعة", false: "عشرة", false: "تسعة"},
+  ),
+];

e5aec1c8e33d557ab9090aba5d31fcaf7df76b11
Aly-exe <alibnraslan@gmail.com>
2026-09-14 20:18:24 +0200

    change package name

--- android/app/src/main/kotlin/com/example/quizapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.alibnraslan.quizapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/new/alyquizapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.new.alyquizapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
